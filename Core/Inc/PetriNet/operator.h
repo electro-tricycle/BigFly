2714d892ed10cf3bc091ea7da0c5e7d8ad0b127f
jay chou <jay chou@LAPTOP-TCI1690P>
2021-07-30 15:32:30 +0800

    ADD OVERLOAD OPERATOR

--- Core/Inc/PetriNet/operator.h
+++ Core/Inc/PetriNet/operator.h
@@ -9,7 +9,19 @@
 #define INC_PETRINET_OPERATOR_H_
 
 
-
+bool operator<(const Eigen::Vector3i &a, const Eigen::Vector3i &b)
+{
+    Eigen::Vector3i res;
+    res = a - b;
+    for (int i = 0; i < a.size(); ++i)
+    {
+        if (res[i] < 0)
+        {
+            return true;
+        }
+    }
+    return false;
+};
 
 
 #endif /* INC_PETRINET_OPERATOR_H_ */
